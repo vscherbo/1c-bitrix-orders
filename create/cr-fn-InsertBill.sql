e6db458614181f92c81837013411693491ae2735
Vladimir Scherbo <vscherbo@gmail.com>
2015-03-13 12:37:20 +0300

    добавлены поля Дополнительно, инфо, Отгрузка, ОтгрузкаКем

--- create/cr-fn-InsertBill.sql
+++ create/cr-fn-InsertBill.sql
@@ -10,23 +10,49 @@ CREATE OR REPLACE FUNCTION fn_insertbill(
   RETURNS record AS
 $BODY$ DECLARE
   ret_bill RECORD;
+  BuyerComment VARCHAR;
+  DeliveryMode VARCHAR;
+  Delivery VARCHAR;
 -- Хозяин=38 - Гараханян
 -- Хозяин=77 - Бондаренко
   inet_bill_owner integer = 77;
 BEGIN
--- Код=223719 - физ. лицо
-WITH inserted AS (
-   INSERT INTO "Счета"
-          ("Код", "фирма", "Хозяин", "№ счета", "Дата счета", "Сумма", "Интернет", "ИнтернетЗаказ", "КодРаботника", "Статус") 
-   VALUES (aFirmCode, 'АРКОМ', inet_bill_owner, fn_GetNewBillNo(inet_bill_owner), CURRENT_DATE, sum, 't', bx_order, aEmpCode, 0 ) 
-   RETURNING * 
-)
-SELECT * INTO ret_bill FROM inserted;
+    SELECT fvalue INTO BuyerComment 
+    FROM devmod.bx_order_feature
+    WHERE "bx_order_Номер" = bx_order
+        AND fname = 'Комментарии покупателя';
 
-RETURN ret_bill;
+    SELECT fvalue INTO DeliveryMode 
+    FROM devmod.bx_order_feature
+    WHERE "bx_order_Номер" = bx_order
+        AND fname = 'Способ доставки';
+
+    IF DeliveryMode = 'Самовывоз' 
+        THEN Delivery := 'Самовывоз';
+        ELSE Delivery := 'Отправка';
+    END IF;
+
+    WITH inserted AS (
+        INSERT INTO "Счета"
+            ("Код", "фирма", "Хозяин", "№ счета", "Дата счета", "Сумма", "Интернет", "ИнтернетЗаказ", "КодРаботника", "Статус", "инфо", "Дополнительно", "Отгрузка", "ОтгрузкаКем") 
+        VALUES (aFirmCode, 'АРКОМ', inet_bill_owner, fn_GetNewBillNo(inet_bill_owner), CURRENT_DATE, sum, 't', bx_order, aEmpCode, 0, 'Автосчёт на заказ с сайта', BuyerComment, Delivery, DeliveryMode)
+    RETURNING * 
+    )
+    SELECT * INTO ret_bill FROM inserted;
+
+    RETURN ret_bill;
 END
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
 ALTER FUNCTION fn_insertbill(numeric, integer, integer, integer)
   OWNER TO arc_energo;
+
+-- С сайта
+-- 1	Самовывоз
+-- 9	Почта России
+-- 5	Междугородний автотранспорт, Почта, Экспресс-почта
+-- 7	Транспортная компания (ж/д, авиа, авто)
+-- 2	Курьер по СПб
+-- 8	Курьерская служба
+-- 6	Иное
