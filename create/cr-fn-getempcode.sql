dcf88d64c4d62d01d43fb340c762f97a5f2a65fa
Vladimir Scherbo <vscherbo@gmail.com>
2015-03-17 13:52:45 +0300

    Added bank attributes ti LegalEntity

--- create/cr-fn-getempcode.sql
+++ create/cr-fn-getempcode.sql
@@ -10,39 +10,82 @@ $BODY$
 declare
     emp RECORD;
     Firm RECORD;
+    INN VARCHAR;
+    KPP VARCHAR;
     Buyer RECORD;
     FirmCode INTEGER;
+    FirmName VARCHAR;
+    Consignee VARCHAR;
+    DeliveryAddress VARCHAR;
+    PersonLocation VARCHAR;
+    Bank VARCHAR;
+    BIK VARCHAR;
+    R_account VARCHAR;
+    K_account VARCHAR;
+    LegalAddress VARCHAR;
 begin
   SELECT "КодРаботника", "Код" into emp from "Работники" where bx_buyer_id = buyer_id;
   IF not found THEN -- (emp is null) THEN -- Работник не найден, создаём
-    --
+    -- 
     RAISE NOTICE 'Работник не найден, создаём. buyer_id=%', buyer_id;
+    SELECT fvalue INTO INN FROM bx_order_feature WHERE "bx_order_Номер" = bx_order_id AND fname = 'ИНН';
+    SELECT fvalue INTO KPP FROM bx_order_feature WHERE "bx_order_Номер" = bx_order_id AND fname = 'КПП';
+    
     SELECT * INTO Buyer FROM devmod.crosstab('SELECT  "bx_order_Номер", fname, fvalue FROM bx_order_feature
                                             WHERE "bx_order_Номер" = ' || bx_order_id || 
-   	' AND fname IN ( ''Адрес доставки'', ''Грузополучатель'', ''Индекс'', ''ИНН'', ''Контактное лицо'', ''Контактный Email'', ''КПП'' ) ORDER BY fname') 
-    AS bx_order_feature("bx_order_Номер" INTEGER, delivery_address varchar, firm_name varchar, zip_code VARCHAR, INN varchar, person VARCHAR, email VARCHAR, KPP varchar);
-    IF (Buyer.inn IS NOT NULL) AND (Buyer.kpp IS NOT NULL) THEN -- юр. лицо
+   	' AND fname IN (
+                    ''Контактное лицо'', 
+                    ''Контактный Email'', 
+                    ''Контактный телефон'')
+                    ORDER BY fname') 
+    AS bx_order_feature("bx_order_Номер" INTEGER, 
+                    person VARCHAR, 
+                    email VARCHAR, 
+                    phone VARCHAR);
+    SELECT fvalue INTO DeliveryAddress FROM bx_order_feature WHERE "bx_order_Номер" = bx_order_id AND fname = 'Адрес доставки';
+    IF not found THEN DeliveryAddress := ''; END IF;
+    
+    IF (INN IS NOT NULL) AND (KPP IS NOT NULL) THEN -- юр. лицо
         --
-        RAISE NOTICE 'Юр. лицо, ИНН=%, КПП=%', Buyer.inn, Buyer.kpp;
-	    SELECT * INTO Firm FROM "Предприятия" WHERE "ИНН" = Buyer.inn AND "КПП" = Buyer.kpp;
+        RAISE NOTICE 'Юр. лицо, ИНН=%, КПП=%', INN, KPP;
+	    SELECT * INTO Firm FROM "Предприятия" WHERE "ИНН" = INN AND "КПП" = KPP;
 	    IF NOT FOUND THEN -- TODO создание предприятия
-            RAISE NOTICE 'Создание предприятия Предприятие=%, ИНН=%, КПП=%', Buyer.firm_name, Buyer.inn, Buyer.kpp;
+	        SELECT fvalue INTO Consignee FROM bx_order_feature WHERE "bx_order_Номер" = bx_order_id AND fname = 'Грузополучатель';
+	        SELECT fvalue INTO FirmName FROM bx_order_feature WHERE "bx_order_Номер" = bx_order_id AND fname = 'Название компании';
+	        RAISE NOTICE 'Создание предприятия Предприятие=%, ИНН=%, КПП=%', FirmName, INN, KPP;
             WITH inserted AS (   
-                INSERT INTO "Предприятия"("Предприятие", "ИНН", "КПП") VALUES (Buyer.firm_name, Buyer.inn, Buyer.kpp) RETURNING "Код"
+                INSERT INTO "Предприятия"("Предприятие", "ИНН", "КПП", "Грузополучатель", "Адрес") 
+                VALUES (FirmName, INN, KPP, Consignee, DeliveryAddress) 
+                RETURNING "Код"
             )
             SELECT inserted."Код" INTO FirmCode FROM inserted;
+            -- Добавить запись в Реквизиты
+            SELECT fvalue INTO Bank FROM bx_order_feature WHERE "bx_order_Номер" = bx_order_id AND fname = 'Банк';
+            SELECT fvalue INTO BIK FROM bx_order_feature WHERE "bx_order_Номер" = bx_order_id AND fname = 'БИК';
+            SELECT fvalue INTO R_account FROM bx_order_feature WHERE "bx_order_Номер" = bx_order_id AND fname = 'Расчетный счет';
+            SELECT fvalue INTO K_account FROM bx_order_feature WHERE "bx_order_Номер" = bx_order_id AND fname = 'КорСчет';
+            SELECT fvalue INTO LegalAddress FROM bx_order_feature WHERE "bx_order_Номер" = bx_order_id AND fname = 'Юридический адрес';
+            INSERT INTO "Реквизиты"("ДатаРкв", "Код", "Название", "ИНН", "КПП", "Банк", "БИК", "Р_счет", "К_счет", "ЮрАдрес", "Грузополучатель")
+                        VALUES(now(), FirmCode, FirmName, INN, KPP, Bank, BIK, R_account, K_account, LegalAddress, Consignee);
+
         ELSE
             FirmCode := Firm."Код";
 	    END IF;
-    ELSIF (Buyer.inn IS NULL) AND (Buyer.kpp IS NULL) THEN -- физ. лицо
+    ELSIF (INN IS NULL) AND (KPP IS NULL) THEN -- физ. лицо
         RAISE NOTICE 'Физ. лицо';
         FirmCode := 223719;
-    ELSIF (Buyer.inn IS NULL) OR (Buyer.kpp IS NULL) THEN -- юр. лицо, неполная информация
-        RAISE NOTICE 'Юр. лицо, неполная информация ИНН=%, КПП=%', coalesce(Buyer.inn, 'не определён'), coalesce(Buyer.kpp, 'не определён');
+        SELECT fvalue INTO PersonLocation FROM bx_order_feature WHERE "bx_order_Номер" = bx_order_id AND fname = 'Местоположение';
+        IF found THEN DeliveryAddress := PersonLocation || ', ' || DeliveryAddress; END IF;
+    ELSIF (INN IS NULL) OR (KPP IS NULL) THEN -- юр. лицо, неполная информация
+        RAISE NOTICE 'Юр. лицо, неполная информация ИНН=%, КПП=%', coalesce(INN, 'не определён'), coalesce(KPP, 'не определён');
     END IF;
 
     WITH inserted AS (
-       insert INTO "Работники" ("КодРаботника", "Код", bx_buyer_id, "Дата", "ФИО", "ЕАдрес") values ((SELECT MAX("КодРаботника")+1 FROM "Работники"), FirmCode, buyer_id, now(), Buyer.person, Buyer.email) RETURNING "КодРаботника", "Код"
+       insert INTO "Работники" ("КодРаботника", "Код", bx_buyer_id, 
+                                "Дата", "ФИО", "Телефон", "ЕАдрес", "Примечание")  
+                                values ((SELECT MAX("КодРаботника")+1 FROM "Работники"), FirmCode, buyer_id, 
+                                now(), Buyer.person, Buyer.phone, Buyer.email, DeliveryAddress) 
+                                RETURNING "КодРаботника", "Код"
     )
     SELECT inserted."КодРаботника", inserted."Код" INTO emp FROM inserted;
     --
