bee8b14929b166f7c6bfe76eb9a5daf69dabb8c4
Vladimir Scherbo <vscherbo@gmail.com>
2016-07-20 17:47:16 +0300

    Срок - через параметр; исправлено exInfo_truncated

--- create/cr-fn-InsertBill.sql
+++ create/cr-fn-InsertBill.sql
@@ -1,13 +1,14 @@
--- Function: fn_insertbill(numeric, integer, integer, integer, character varying)
+-- Function: fn_insertbill(numeric, integer, integer, integer, character varying, character varying)
 
--- DROP FUNCTION fn_insertbill(numeric, integer, integer, integer, character varying);
+-- DROP FUNCTION fn_insertbill(numeric, integer, integer, integer, character varying, character varying);
 
 CREATE OR REPLACE FUNCTION fn_insertbill(
     sum numeric,
     bx_order integer,
-    acode integer, -- Код предприятия
+    acode integer,
     aempcode integer,
-    ourfirm character varying)
+    ourfirm character varying,
+    orderprocessingtime character varying)
   RETURNS record AS
 $BODY$ DECLARE
   ret_bill RECORD;
@@ -20,9 +21,8 @@ $BODY$ DECLARE
   exInfo_truncated VARCHAR;
 -- Хозяин=38 - Гараханян
 -- Хозяин=77 - Бондаренко
-  inet_bill_owner integer = 77;
+  inet_bill_owner integer;
   Max_ExtraInfo CONSTANT INTEGER := 250;
-  loc_OrderProcessingTime varchar;
   bill_no INTEGER;
 BEGIN
     SELECT fvalue INTO BuyerComment FROM bx_order_feature WHERE "bx_order_Номер" = bx_order AND fname = 'Комментарии покупателя';
@@ -33,21 +33,28 @@ BEGIN
     IF found THEN ExtraInfo := ExtraInfo || ' Название службы доставки:' || DeliveryService; END IF;
     IF length(ExtraInfo) > Max_ExtraInfo  THEN
        exInfo_truncated = rpad(ExtraInfo, Max_ExtraInfo);
-       RAISE NOTICE 'ExtraInfo % longer than %, truncated to=%', ExtraInfo,Max_ExtraInfo,exInfo_truncated;
+       RAISE NOTICE 'ExtraInfo % longer than %, truncated.', ExtraInfo,Max_ExtraInfo;
+    ELSE
+       exInfo_truncated = ExtraInfo;
     END IF;
+    RAISE NOTICE 'exInfo_truncated=%', exInfo_truncated;
 
     SELECT fvalue INTO DeliveryMode FROM bx_order_feature WHERE "bx_order_Номер" = bx_order AND fname = 'Способ доставки';
 
-    IF DeliveryMode = 'Самовывоз' THEN Delivery := 'Самовывоз'; ELSE Delivery := 'Отправка'; END IF;
+    IF DeliveryMode = 'Самовывоз' THEN 
+       Delivery := 'Самовывоз'; 
+       DeliveryMode = NULL; -- Важно! для формирования счёт-факса
+    ELSE
+       Delivery := 'Отправка'; 
+    END IF;
     
-    SELECT Order_ProcessingTime() INTO loc_OrderProcessingTime;
+    -- SELECT Order_ProcessingTime() INTO loc_OrderProcessingTime;
     inet_bill_owner := get_bill_owner_by_entcode(aCode);
     bill_no := fn_GetNewBillNo(inet_bill_owner);
     WITH inserted AS (
         INSERT INTO "Счета"
             ("Код", "фирма", "Хозяин", "№ счета", "предок", "Дата счета", "Сумма", "Интернет", "ИнтернетЗаказ", "КодРаботника", "Статус", "инфо", "Дополнительно", "Отгрузка", "ОтгрузкаКем", "Срок") 
-        VALUES (acode, ourFirm, inet_bill_owner, bill_no, bill_no, CURRENT_DATE, sum, 't', bx_order, aEmpCode, 0, 'Автосчёт на заказ с сайта', exInfo_truncated, Delivery, DeliveryMode, loc_OrderProcessingTime)
-        -- VALUES (aFirmCode, 'АРКОМ', inet_bill_owner, bill_no, bill_no, CURRENT_DATE, sum, 't', bx_order, aEmpCode, 0, 'Автосчёт на заказ с сайта', exInfo_truncated, Delivery, DeliveryMode, loc_OrderProcessingTime)
+        VALUES (acode, ourFirm, inet_bill_owner, bill_no, bill_no, CURRENT_DATE, sum, 't', bx_order, aEmpCode, 0, 'Автосчёт на заказ с сайта', exInfo_truncated, Delivery, DeliveryMode, OrderProcessingTime)
     RETURNING * 
     )
     SELECT * INTO ret_bill FROM inserted;
@@ -57,9 +64,9 @@ END
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-ALTER FUNCTION fn_insertbill(numeric, integer, integer, integer, character varying)
+ALTER FUNCTION fn_insertbill(numeric, integer, integer, integer, character varying, character varying)
   OWNER TO arc_energo;
-COMMENT ON FUNCTION fn_insertbill(numeric, integer, integer, integer, character varying) IS 'С сайта ''Способ доставки''
+COMMENT ON FUNCTION fn_insertbill(numeric, integer, integer, integer, character varying, character varying) IS 'С сайта ''Способ доставки''
 1    Самовывоз
 9    Почта России
 5    Междугородний автотранспорт, Почта, Экспресс-почта
