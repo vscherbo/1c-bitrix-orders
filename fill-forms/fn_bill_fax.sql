6cd533a70dc5e60f5b9a0c43b95f21bdcf3db94a
Vladimir Scherbo <vscherbo@gmail.com>
2016-07-28 14:31:59 +0300

    запрос полей выше запроса позиций

--- fill-forms/fn_bill_fax.sql
+++ fill-forms/fn_bill_fax.sql
@@ -15,6 +15,61 @@ import locale
 import re
 from os.path import expanduser
 
+bill_fax_fields_query = """
+SELECT 
+r."Ф_НазваниеКратко" pg_firm
+, s."ЗаДиректора" pg_signature
+, s."ВидНомерДатаДокументаДир" pg_proxy_doc
+, r."Ф_НазваниеПолное" pg_firm_full_name
+, r."Ф_ПочтовыйАдрес" pg_post_address
+, r."Ф_ФактическийАдрес" pg_fact_address
+, r."Ф_Телефон" pg_citi_phone
+, trim(f."ПрефиксВСчет") pg_prefix
+, f."Ф_ИНН" pg_inn
+, r."Ф_КПП" pg_kpp
+, r."Ф_РассчетныйСчет" pg_account
+, r."Ф_Банк" pg_bank
+, "Ф_КоррСчет" pg_corresp
+, "Ф_БИК" pg_bik
+,to_char(b."Сумма", '999 999D99') pg_price
+, to_char(b."№ счета", '9999-9999') AS pg_order
+, to_char(b."Дата счета", 'DD.MM.YYYY') pg_order_date
+, b."ставкаНДС"::VARCHAR pg_vat
+, b."Дополнительно" pg_add
+, COALESCE (quote_literal(b."ОтгрузкаКем") || '. Оплата доставки при получении', 'Самовывоз') pg_carrier
+, e.email pg_email
+, e.telephone pg_phone
+, e."ФИО" pg_mgr_name
+, c."ЮрНазвание" pg_firm_buyer
+, c."Факс" pg_fax
+, b."фирма" pg_firm_key 
+FROM "Счета" b
+JOIN "Фирма" f ON b."фирма" = f."КлючФирмы"
+JOIN "ФирмаРеквизиты" r ON b."фирма" = r."КодФирмы" AND r."Ф_Активность" = TRUE
+JOIN "Сотрудники" e ON b."Хозяин" = e."Менеджер"
+JOIN "Предприятия" c ON c."Код" = b."Код"
+JOIN (SELECT "ДатаСтартаПодписи", "КодОтчета", "НомерСотрудника", "КлючФирмы", "ЗаДиректора", "ВидНомерДатаДокументаДир" 
+    FROM "Подписи" 
+    WHERE "КодОтчета" = 'СчетФакс' 
+          AND "НомерСотрудника" = 0 
+          AND "КлючФирмы" = (SELECT "фирма" FROM "Счета" WHERE "№ счета" = """  + str(bill_no) + """)
+    ORDER BY "ДатаСтартаПодписи" DESC LIMIT 1 ) AS s ON s."КлючФирмы" = b."фирма"
+WHERE 
+b."№ счета" = 
+""" + str(bill_no) + ";"
+
+recs = plpy.execute(bill_fax_fields_query)
+if 0 == recs.nrows():
+    return 'Not found'
+
+upd_dict = {}
+for (k, v) in recs[0].items():
+    if v is None:
+        upd_dict[k] = ''
+    else:
+        #plpy.log('[' + v + ']')
+        upd_dict[k] = v.decode('utf-8')
+
 order_items_query = """
 SELECT
 c."ПозицияСчета"::VARCHAR pg_position
@@ -78,60 +133,6 @@ total_words = total_pos_in_words.split(' ')
 total_pos_in_words = total_words[0].capitalize() + ' ' + ' '.join(total_words[1:])
 
 doc.save(outfile)
-
-bill_fax_fields_query = """
-SELECT 
-r."Ф_НазваниеКратко" pg_firm
-, s."ЗаДиректора" pg_signature
-, s."ВидНомерДатаДокументаДир" pg_proxy_doc
-, r."Ф_НазваниеПолное" pg_firm_full_name
-, r."Ф_ПочтовыйАдрес" pg_post_address
-, r."Ф_ФактическийАдрес" pg_fact_address
-, r."Ф_Телефон" pg_citi_phone
-, trim(f."ПрефиксВСчет") pg_prefix
-, f."Ф_ИНН" pg_inn
-, r."Ф_КПП" pg_kpp
-, r."Ф_РассчетныйСчет" pg_account
-, r."Ф_Банк" pg_bank
-, "Ф_КоррСчет" pg_corresp
-, "Ф_БИК" pg_bik
-,to_char(b."Сумма", '999 999D99') pg_price
-, to_char(b."№ счета", '9999-9999') AS pg_order
-, to_char(b."Дата счета", 'DD.MM.YYYY') pg_order_date
-, b."ставкаНДС"::VARCHAR pg_vat
-, b."Дополнительно" pg_add
-, COALESCE (quote_literal(b."ОтгрузкаКем") || '. Оплата доставки при получении', 'Самовывоз') pg_carrier
-, e.email pg_email
-, e.telephone pg_phone
-, e."ФИО" pg_mgr_name
-, c."ЮрНазвание" pg_firm_buyer
-, c."Факс" pg_fax
-FROM "Счета" b
-JOIN "Фирма" f ON b."фирма" = f."КлючФирмы"
-JOIN "ФирмаРеквизиты" r ON b."фирма" = r."КодФирмы" AND r."Ф_Активность" = TRUE
-JOIN "Сотрудники" e ON b."Хозяин" = e."Менеджер"
-JOIN "Предприятия" c ON c."Код" = b."Код"
-JOIN (SELECT "ДатаСтартаПодписи", "КодОтчета", "НомерСотрудника", "КлючФирмы", "ЗаДиректора", "ВидНомерДатаДокументаДир" 
-    FROM "Подписи" 
-    WHERE "КодОтчета" = 'СчетФакс' 
-          AND "НомерСотрудника" = 0 
-          AND "КлючФирмы" = (SELECT "фирма" FROM "Счета" WHERE "№ счета" = """  + str(bill_no) + """)
-    ORDER BY "ДатаСтартаПодписи" DESC LIMIT 1 ) AS s ON s."КлючФирмы" = b."фирма"
-WHERE 
-b."№ счета" = 
-""" + str(bill_no) + ";"
-
-recs = plpy.execute(bill_fax_fields_query)
-if 0 == recs.nrows():
-    return 'Not found'
-
-upd_dict = {}
-for (k, v) in recs[0].items():
-    if v is None:
-        upd_dict[k] = ''
-    else:
-        #plpy.log('[' + v + ']')
-        upd_dict[k] = v.decode('utf-8')
 
 obj = UserFields(outfile, outfile)
 obj.update(upd_dict)
