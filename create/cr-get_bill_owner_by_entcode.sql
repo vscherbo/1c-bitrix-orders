d3855640a96258a9368aed14fe22e14e3ac7b498
Vladimir Scherbo <vscherbo@gmail.com>
2016-09-15 12:53:23 +0300

    с учётом закрепления менеджеров

--- create/cr-get_bill_owner_by_entcode.sql
+++ create/cr-get_bill_owner_by_entcode.sql
@@ -4,10 +4,28 @@
 
 CREATE OR REPLACE FUNCTION get_bill_owner_by_entcode(entcode integer)
   RETURNS integer AS
-$BODY$BEGIN
-  RETURN 44; -- 44-АГС, 77-ВВ
-END;$BODY$
-  LANGUAGE plpgsql VOLATILE
+$BODY$
+    SELECT d."Менеджер" as result FROM
+        (SELECT "Менеджер"
+            FROM 
+            "ДилерыМенеджеров" dm
+            WHERE 
+                "Код" = entcode
+                AND "Код" IN (SELECT "Код" FROM "vwДилеры")
+                AND "Менеджер" IN (SELECT "Менеджер" 
+                                    FROM "Сотрудники"
+                                    WHERE "Менеджер" IS NOT NULL
+                                    AND "МенеджерСтат" = TRUE
+                                    AND "Активность" = TRUE
+                )
+            ORDER BY "Приоритет" LIMIT 1) d
+    UNION
+        SELECT 38 as result -- Арутюн
+            FROM "Предприятия" 
+            WHERE entcode NOT IN (SELECT "Код" FROM "vwДилеры");
+--  RETURN 44; -- 44-АГС, 77-ВВ
+$BODY$
+  LANGUAGE sql VOLATILE
   COST 100;
 ALTER FUNCTION get_bill_owner_by_entcode(integer)
   OWNER TO arc_energo;
