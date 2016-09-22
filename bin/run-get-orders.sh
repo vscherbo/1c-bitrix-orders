72e25e66b6d5e75a7868ccc56c46f6acb65e07ce
Vladimir Scherbo <vscherbo@gmail.com>
2016-09-22 09:20:30 +0300

    добавлен вывод метки времени при срабатывании pkill

--- bin/run-get-orders.sh
+++ bin/run-get-orders.sh
@@ -4,6 +4,11 @@ do_loop=1
 
 while [ "+1" == +$do_loop ]
 do
-   bin/get-orders.py
+   /usr/bin/pkill -f 'python.*get-orders\.py'
+   if [ $? -eq 0 ]
+   then
+      echo `date "+%F_%H-%M-%S"`
+   fi
+   bin/get-orders.py --log DEBUG &
    sleep 60
 done
