0e2aceb74c3c01f85f4c561f1302a6880b84cc83
Vladimir Scherbo <vscherbo@gmail.com>
2016-09-22 09:21:03 +0300

    поиск и проверка предприятия по ИНН

---
+++ create/cr-verify_KPP_by_INN.sql
@@ -0,0 +1,30 @@
+-- Function: verify_kpp_by_inn(character varying)
+
+-- DROP FUNCTION verify_kpp_by_inn(character varying);
+
+CREATE OR REPLACE FUNCTION verify_kpp_by_inn(inn character varying)
+  RETURNS character varying AS
+$BODY$DECLARE
+  rec_answer record;
+  ret_kpp varchar := 'N/A';
+  rec_kpp record;
+begin
+  rec_answer := get_reqs_by_INN(INN);
+  IF rec_answer.ret_flg THEN
+      rec_kpp := parse_KPP(rec_answer.ret_txt);
+      IF rec_kpp.ret_kpp_flg THEN
+        ret_kpp := rec_kpp.ret_kpp;
+      ELSE
+         RAISE NOTICE 'verify_kpp_by_inn: не удалось извлечь КПП из ответа сайта ИТС, ret_kpp=%', rec_kpp.ret_kpp;
+      END IF;
+  ELSE
+     RAISE NOTICE 'verify_kpp_by_inn: не получен ответ от сайта ИТС, ret_txt=%', rec_answer.ret_txt;
+  END IF;
+
+  RETURN ret_kpp;
+end
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION verify_kpp_by_inn(character varying)
+  OWNER TO arc_energo;
---
+++ create/cr_find_enterprise.sql
@@ -0,0 +1,40 @@
+-- Function: fn_find_enterprise(character varying, character varying)
+
+-- DROP FUNCTION fn_find_enterprise(character varying, character varying);
+
+CREATE OR REPLACE FUNCTION fn_find_enterprise(
+    inn character varying,
+    kpp character varying)
+  RETURNS record AS
+$BODY$
+declare
+    Firm RECORD;
+    chk_KPP VARCHAR;
+begin
+    IF INN IS NULL THEN RAISE 'fn_find_enterprise: INN is NULL'; END IF;
+    IF KPP IS NULL THEN RAISE 'fn_find_enterprise: KPP is NULL'; END IF;
+    SELECT * INTO Firm FROM "Предприятия" WHERE "ИНН" = INN AND "КПП" = KPP;
+    IF NOT FOUND THEN -- проверка КПП по ИНН
+        RAISE NOTICE 'fn_find_enterprise: Предприятие не найдено INN=%, KPP=%', INN, KPP;
+        chk_KPP := verify_KPP_by_INN(INN);
+        IF chk_KPP <> 'N/A' THEN -- получен ответ с сайте ИТС 1С
+            IF chk_KPP <> KPP THEN -- КПП с сайта ИТС 1С отличается от КПП из заказа
+               RAISE NOTICE 'fn_find_enterprise: Предприятие найдено в Инете по INN=%, chk_KPP=%', INN, chk_KPP;
+               SELECT * INTO Firm FROM "Предприятия" WHERE "ИНН" = INN AND "КПП" = chk_KPP;
+               IF FOUND THEN -- TODO IF FOUND -> исправить КПП на сайте?
+                   RAISE NOTICE 'fn_find_enterprise: Предприятие найдено в БД по INN=%, chk_KPP=%', INN, chk_KPP;
+               END IF;
+            ELSE
+               RAISE NOTICE 'fn_find_enterprise: получен тот же КПП с сайта ИТС 1С по INN=%, KPP=%', INN, chk_KPP;
+            END IF; -- КПП с сайта ИТС 1С отличается от КПП из заказа
+        ELSE
+           RAISE NOTICE 'fn_find_enterprise: Нет ответа с сайта ИТС 1С по INN=%, KPP=%', INN, chk_KPP;
+        END IF; -- получен ответ с сайте ИТС 1С
+    END IF; -- if found
+    RETURN Firm;
+end
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION fn_find_enterprise(character varying, character varying)
+  OWNER TO arc_energo;
