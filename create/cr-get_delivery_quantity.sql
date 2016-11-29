d96a67a462520e3f5c2492ccfe32d6d5c66a523c
Vladimir Scherbo <vscherbo@gmail.com>
2016-11-29 17:21:28 +0300

    функция возвращает строку срок-количество из заказа

---
+++ create/cr-get_delivery_quantity.sql
@@ -0,0 +1,14 @@
+-- Function: get_delivery_quantity(text)
+
+-- DROP FUNCTION get_delivery_quantity(text);
+
+CREATE OR REPLACE FUNCTION get_delivery_quantity(order_item_id text)
+  RETURNS text AS
+$BODY$
+    SELECT fvalue AS RESULT FROM bx_order_item_feature WHERE bx_order_item_id = order_item_id AND fname = 'Срок-Количество';
+$BODY$
+  LANGUAGE sql VOLATILE
+  COST 100;
+ALTER FUNCTION get_delivery_quantity(text)
+  OWNER TO arc_energo;
+
