9face7a430c17bb4a7a0f081a7c76a1a9f586116
Vladimir Scherbo <vscherbo@gmail.com>
2016-11-29 12:26:32 +0300

    добавлен аргумент KS, пока не используется

--- create/cr-orderitem_processingtime.sql
+++ create/cr-orderitem_processingtime.sql
@@ -1,8 +1,8 @@
--- Function: orderitem_processingtime()
+-- Function: orderitem_processingtime(integer)
 
--- DROP FUNCTION orderitem_processingtime();
+-- DROP FUNCTION orderitem_processingtime(integer);
 
-CREATE OR REPLACE FUNCTION orderitem_processingtime()
+CREATE OR REPLACE FUNCTION orderitem_processingtime(ks integer)
   RETURNS character varying AS
 $BODY$  -- ignore KS for a while
 SELECT "Значение" 
@@ -11,6 +11,6 @@ WHERE "КодЭнциклопедии" = 7 AND "КодСтатьи" = 11
  $BODY$
   LANGUAGE sql VOLATILE
   COST 100;
-ALTER FUNCTION orderitem_processingtime()
+ALTER FUNCTION orderitem_processingtime(integer)
   OWNER TO arc_energo;
-COMMENT ON FUNCTION orderitem_processingtime() IS 'Возвращает срок обработки позиции заказа (содержание счёта)';
+COMMENT ON FUNCTION orderitem_processingtime(integer) IS 'Возвращает срок обработки позиции заказа (содержание счёта)';
