cab60de50edb43a21c0796d3c72115c5d13e1faa
Vladimir Scherbo <vscherbo@gmail.com>
2016-11-29 17:30:19 +0300

    снова проверка 2-х складов: Ясная и Выставка

--- create/cr-is_in_stock.sql
+++ create/cr-is_in_stock.sql
@@ -1,20 +1,22 @@
--- Function: is_in_stock(integer, numeric)
+-- Function: is_in_stock(integer)
 
-DROP FUNCTION is_in_stock(integer);
+-- DROP FUNCTION is_in_stock(integer);
 
-CREATE OR REPLACE FUNCTION is_in_stock(
-    "KS" integer)
-  RETURNS TABLE(wh_id INTEGER, wh_qnt NUMERIC(18,3)) AS
+CREATE OR REPLACE FUNCTION is_in_stock(IN "KS" integer)
+  RETURNS TABLE(wh_id integer, wh_qnt numeric) AS
 $BODY$
 DECLARE loc_in_stock numeric;
 BEGIN
  RETURN QUERY SELECT "КодСклада", SUM(("НаСкладе" - COALESCE("Рез", 0))::NUMERIC) -- INTO loc_in_stock
    FROM "vwСкладВсеПодробно"
    WHERE "КодСодержания" = "KS"
-   AND "КодСклада" IN (2) -- Ясная
-   -- AND "КодСклада" IN (2, 5) -- Ясная, Выставка
+   AND "КодСклада" IN (2, 5) -- Ясная, Выставка
    GROUP BY "КодСклада"
    ;
 END;$BODY$
   LANGUAGE plpgsql VOLATILE
-  COST 100;
+  COST 100
+  ROWS 2;
+ALTER FUNCTION is_in_stock(integer)
+  OWNER TO arc_energo;
+
