2b791a9282416b0ba5080fccd9082167142cc93a
Vladimir Scherbo <vscherbo@gmail.com>
2016-12-23 10:16:33 +0300

    разный поиск в зависимости от длины ИНН

--- create/cr_find_enterprise.sql
+++ create/cr_find_enterprise.sql
@@ -10,10 +10,18 @@ $BODY$
 declare
     Firm RECORD;
     chk_KPP VARCHAR;
+    len_inn INTEGER;
 begin
     IF INN IS NULL THEN RAISE 'fn_find_enterprise: INN is NULL'; END IF;
     IF KPP IS NULL THEN RAISE 'fn_find_enterprise: KPP is NULL'; END IF;
-    SELECT * INTO Firm FROM "Предприятия" WHERE "ИНН" = INN AND "КПП" = KPP;
+    len_inn := length(INN);
+    IF 10 = len_inn THEN
+        SELECT * INTO Firm FROM "Предприятия" WHERE "ИНН" = INN AND "КПП" = KPP;
+    ELSIF 12 = len_inn THEN
+        SELECT * INTO Firm FROM "Предприятия" WHERE "ИНН" = INN;
+    ELSE
+        RAISE 'Непредвиденная длина ИНН=%, ИНН=% ', len_inn, INN;
+    END IF; -- length(INN)
     IF NOT FOUND THEN -- проверка КПП по ИНН
         RAISE NOTICE 'fn_find_enterprise: Предприятие не найдено INN=%, KPP=%', INN, KPP;
         chk_KPP := verify_KPP_by_INN(INN);
