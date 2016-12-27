2164442d327ec18e9ed03647760bd5d4d8abc191
Vladimir Scherbo <vscherbo@gmail.com>
2016-12-27 13:21:31 +0300

    исправлена ошибка (== вместо =) в IF

--- create/cr-setup_reserve.sql
+++ create/cr-setup_reserve.sql
@@ -14,7 +14,7 @@ BEGIN
 
 SELECT Coalesce(ОКЕИ,796) INTO loc_okei FROM arc_energo."Содержание" WHERE "КодСодержания"=ks;
 	
-IF 796 == loc_okei THEN  -- штучный
+IF 796 = loc_okei THEN  -- штучный
     RAISE NOTICE 'ШТУЧНЫЙ %', loc_okei;
     loc_qnt := setup_reserve_item(bill_no, ks, qnt);
     RAISE NOTICE 'отработала setup_reserve_item, не удалось поставить в резерв %', loc_qnt;
