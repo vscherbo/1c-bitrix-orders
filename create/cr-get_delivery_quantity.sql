10b37adf08c27c515f62ebd33cdef5091d43c641
Vladimir Scherbo <vscherbo@gmail.com>
2017-04-13 14:56:47 +0300

    изменено имя аргумента функции

--- create/cr-get_delivery_quantity.sql
+++ create/cr-get_delivery_quantity.sql
@@ -2,10 +2,10 @@
 
 -- DROP FUNCTION get_delivery_quantity(text);
 
-CREATE OR REPLACE FUNCTION get_delivery_quantity(order_item_id text)
+CREATE OR REPLACE FUNCTION get_delivery_quantity(arg_order_item_id text)
   RETURNS text AS
 $BODY$
-    SELECT fvalue AS RESULT FROM bx_order_item_feature WHERE bx_order_item_id = order_item_id AND fname = 'Срок-Количество';
+    SELECT fvalue AS RESULT FROM bx_order_item_feature WHERE bx_order_item_id = arg_order_item_id AND fname = 'Срок-Количество';
 $BODY$
   LANGUAGE sql VOLATILE
   COST 100;
