bc1194a8ea24de4f2111eb9e5a6f89075a60cd02
Vladimir Scherbo <vscherbo@gmail.com>
2017-04-20 09:41:02 +0300

    добавлен вх. параметр - номер заказа для поиска свойства срок-количества

--- create/cr-get_delivery_quantity.sql
+++ create/cr-get_delivery_quantity.sql
@@ -1,14 +1,20 @@
--- Function: get_delivery_quantity(text)
+-- Function: get_delivery_quantity(integer, text)
 
--- DROP FUNCTION get_delivery_quantity(text);
+-- DROP FUNCTION get_delivery_quantity(integer, text);
 
-CREATE OR REPLACE FUNCTION get_delivery_quantity(arg_order_item_id text)
+CREATE OR REPLACE FUNCTION get_delivery_quantity(
+arg_order_id integer,
+arg_order_item_id text)
   RETURNS text AS
 $BODY$
-    SELECT fvalue AS RESULT FROM bx_order_item_feature WHERE bx_order_item_id = arg_order_item_id AND fname = 'Срок-Количество';
+    SELECT fvalue AS RESULT 
+           FROM bx_order_item_feature 
+           WHERE "bx_order_Номер" = arg_order_id
+                 AND bx_order_item_id = arg_order_item_id 
+                 AND fname = 'Срок-Количество';
 $BODY$
   LANGUAGE sql VOLATILE
   COST 100;
-ALTER FUNCTION get_delivery_quantity(text)
+ALTER FUNCTION get_delivery_quantity(integer, text)
   OWNER TO arc_energo;
 
