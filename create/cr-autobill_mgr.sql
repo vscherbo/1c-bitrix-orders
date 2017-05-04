26e22775071ef5e2bb68ed77de0d58a7df369b78
Vladimir Scherbo <vscherbo@gmail.com>
2017-05-04 12:04:21 +0300

    добавлена диагностика

--- create/cr-autobill_mgr.sql
+++ create/cr-autobill_mgr.sql
@@ -8,7 +8,8 @@ $BODY$
 declare 
    loc_mgr_code INTEGER;
 BEGIN
-	IF arg_code = 41 THEN -- get an autobill active manager
+	IF arg_code = 41 THEN -- returns an autobill active manager
+	   -- SELECT const_value INTO loc_mgr_code FROM arc_constants WHERE const_name = 'autobill_mgr';
 	   SELECT "Менеджер" INTO loc_mgr_code
             FROM "ДилерыМенеджеров"
             WHERE "Код" = -1
@@ -18,8 +19,10 @@ BEGIN
                                     AND "МенеджерСтат" = TRUE
                                     AND "Активность" = TRUE)
             ORDER BY "Приоритет" LIMIT 1;   
+       RAISE NOTICE 'выбран менеджер автосчетов=%', loc_mgr_code;
 	ELSE
 	   loc_mgr_code := arg_code;
+       RAISE NOTICE 'не менеджер-41, не меняем менеджера=%', loc_mgr_code;
 	END IF;
 	RETURN loc_mgr_code;
 end
