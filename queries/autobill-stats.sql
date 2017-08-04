02e0d38480c8cd1971e98108bce6ae44d7e3ac5e
Vladimir Scherbo <vscherbo@gmail.com>
2017-08-04 13:39:34 +0300

    отчёт по автосчетам с подсчётом количества

--- queries/autobill-stats.sql
+++ queries/autobill-stats.sql
@@ -1,12 +1,26 @@
-\echo 'Автосчёт не создан'
-\echo
+\set QUIET on
+\t on
+select 'Автосчёт не создан: ' || count(*) 
+FROM arc_energo.bx_order bo 
+where 
+bo.billcreated not in (1,2,6,7) -- прочее, ошибки
+and dt_insert > now()- '1 day'::INTERVAL;
+\t off
 SELECT bx_order_no AS "Заказ", descr AS "Описание"
             FROM aub_log
             where res_code NOT IN (1,2,6,7) AND mod_id = '-1' AND dt_insert > now()- '1 day'::INTERVAL
             ORDER BY id;
 
-\echo 'Создан частичный автосчёт'
+
+
 \echo
+\t on
+select 'Создан частичный автосчёт: ' || count(*) 
+FROM arc_energo.bx_order bo 
+where 
+bo.billcreated in (2,6,7) -- частичный
+and dt_insert > now()- '1 day'::INTERVAL;
+\t off
 SELECT bx_order_no AS "Заказ", descr AS "Описание"
 FROM aub_log 
     WHERE bx_order_no IN
@@ -16,7 +30,16 @@ FROM aub_log
 AND res_code IS NOT NULL            
 ORDER BY id;
 
-\echo 'Автосчёт создан'
+
+
+\echo
+\t on
+select 'Автосчёт создан: ' || count(*) 
+FROM arc_energo.bx_order bo 
+where 
+bo.billcreated = 1 -- полный
+and dt_insert > now()- '1 day'::INTERVAL;
+\t off
 \echo
 SELECT bx_order_no AS "Заказ", descr AS "Описание"
 FROM aub_log
