8e60d6760d61bd4d8a8c3c542d05416cd57d91a7
Vladimir Scherbo <vscherbo@gmail.com>
2017-08-21 16:33:20 +0300

    добавлен trim для фискального названия товара

--- create/cr-fiscal-name.sql
+++ create/cr-fiscal-name.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION arc_energo.fiscal_name(arg_mod_id text)
  RETURNS text
  LANGUAGE sql
 AS $function$
-SELECT format('%s %s %s', m.mod_id, d.dev_name_long, d.dev_name) AS result
+SELECT format('%s %s %s', m.mod_id, trim(from d.dev_name_long), trim(from d.dev_name)) AS result
 FROM modifications m
 JOIN device d ON m.dev_id = d.dev_id AND d.version_num=1
 WHERE m.version_num=1 AND mod_id = arg_mod_id;
