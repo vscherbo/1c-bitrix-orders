a969e6c302747809ae4e042f5eb2835714e1ac31
Vladimir Scherbo <vscherbo@gmail.com>
2017-08-21 16:41:34 +0300

    уточнение критериев

--- create/cr-is_bx_order_valid.sql
+++ create/cr-is_bx_order_valid.sql
@@ -1,5 +1,7 @@
+-- DROP FUNCTION is_bx_order_valid(integer);
+
 CREATE OR REPLACE FUNCTION is_bx_order_valid(bx_order_no integer)
-  RETURNS BOOLEAN AS
+RETURNS boolean AS
 $BODY$
 DECLARE
     o RECORD;
@@ -16,7 +18,14 @@ SELECT bo.*, bb.bx_name, bf.fvalue AS email
     WHERE
         bo."Номер" = bx_order_no
         AND bo.bx_buyer_id = bb.bx_buyer_id
-        AND (bo."Номер" = bf."bx_order_Номер" AND bf.fname = 'EMail');
+        AND (bo."Номер" = bf."bx_order_Номер" AND bf.fname = 'EMail')
+UNION
+SELECT bo.*, bb.bx_name, bf.fvalue AS email
+    FROM vw_bx_actual_order bo, bx_buyer bb, bx_order_feature bf
+    WHERE
+        bo."Номер" = bx_order_no
+        AND bo.bx_buyer_id = bb.bx_buyer_id
+        AND (bo."Номер" = bf."bx_order_Номер" AND bf.fname = 'EMail покупателя');
 
 RETURN FOUND;
 END;$BODY$
