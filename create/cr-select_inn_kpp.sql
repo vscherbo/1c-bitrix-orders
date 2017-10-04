b5e19659c2a2125a45fe631724c12d0abc5ab4ad
Vladimir Scherbo <vscherbo@gmail.com>
2017-10-04 14:55:21 +0300

    рабочая версия

--- create/cr-select_inn_kpp.sql
+++ create/cr-select_inn_kpp.sql
@@ -1,13 +1,20 @@
 --DROP FUNCTION arc_energo.select_inn_kpp(text, text);
 CREATE OR REPLACE FUNCTION arc_energo.select_inn_kpp(arg_inn text, arg_kpp text)
- RETURNS setof record
+ RETURNS SETOF record
  LANGUAGE plpgsql
 AS $function$
 declare
 loc_select text := 'SELECT "Код" FROM "Предприятия"';
+ret_firmcode record;
 BEGIN
 loc_select := concat_ws(' ', loc_select, 'WHERE', '"ИНН" = ' || quote_literal(arg_INN), 'and "КПП" = ' || quote_literal(arg_KPP), ';');
-BEGIN
-    EXECUTE loc_select; 
+-- raise notice 'loc_select=%', loc_select;
+begin
+    FOR ret_firmcode IN EXECUTE loc_select
+    loop
+        RETURN NEXT ret_firmcode;
+    END LOOP;
+    return;
 END;    
-end;$function$;
+end;$function$
+
