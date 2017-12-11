4d864a97298809bbaa7bc94e3bceb6b61776c3ef
Vladimir Scherbo <vscherbo@gmail.com>
2017-12-11 16:24:28 +0300

    работающий вариант от xml-файла до автосчёта

--- bin/bx_orders_download.py
+++ bin/bx_orders_download.py
@@ -3,149 +3,142 @@
 
 import os
 from requests import Request, Session
-from sys import exit
-import codecs
+# from sys import exit
+from codecs import open
 import time
 import re
 import logging
 
 
-escaped = re.compile(u'&(?!quot;|lt;|amp;|gt;|apos;)')
-
 def get_xml_list_fixed(inp_text):
-    global escaped
-    xml_orders = []
-    for l1 in inp_text:
-        l = re.sub(u'windows\-1251', u'UTF-8', l1).lstrip()
-        esc_found = escaped.search(l)
+    escaped = re.compile(u'&(?!quot;|lt;|amp;|gt;|apos;)')
+    loc_xml_orders = []
+    for inp_line in inp_text:
+        utf8_line = re.sub(u'windows-1251', u'UTF-8', inp_line).lstrip()
+        esc_found = escaped.search(utf8_line)
         if esc_found:
-            broken_fixed = re.sub(r'&(\w+)', '', l )
+            broken_fixed = re.sub(r'&(\w+)', '', utf8_line)
             if broken_fixed:
-                xml_orders.append(broken_fixed)
+                loc_xml_orders.append(broken_fixed)
         else:
-           xml_orders.append(l)
-    return xml_orders
+            loc_xml_orders.append(utf8_line)
+    return loc_xml_orders
 
-def elem2str(aLabel, aTag, aText, sql_flds, sql_vals):
-    if None != aText and u'\n' != aText:
-       rstr = aLabel + aTag + u'=' + aText +u'\n'
-       sql_flds.append(' "' + aTag.replace(' ', '_') + u'"')
-       sql_vals.append(' \'' + aText.replace('\'', '\'\'') + u'\'')
-    else:
-       rstr = u''
-    return rstr
+def get_orders(site, site_user, site_pword, version):
+    url = proto + site + '/bitrix/admin/1c_exchange.php'
 
-################## Main ###################################
+    sess = Session()
+    sess.headers['Connection'] = 'close'
+    sess.verify = verify_flag
+    sess.keep_alive = True
 
-# TODO command line options: conf file, logging_level, order's status file
-log_dir = ''
-log_format = '[%(filename)-20s:%(lineno)4s - %(funcName)20s()] %(levelname)-7s | %(asctime)-15s | %(message)s'
-
-(prg_name, prg_ext) = os.path.splitext(os.path.basename(__file__))
-logging.basicConfig(filename=prg_name+'.log', filemode = 'a', format=log_format, level=logging.INFO)
-
-
-conf = {}
-execfile(prg_name+".conf", conf)
-
-if conf['site'].endswith('arc.world'):
-   verify_flag = False
-   proto = 'http://'
-else:
-   verify_flag = True
-   proto = 'https://'
-
-re_ver_num = re.compile(r'<НомерВерсии>(\d+)</НомерВерсии>')
-
-url = proto + conf['site'] + '/bitrix/admin/1c_exchange.php'
-
-sess = Session()
-sess.headers['Connection'] = 'close'
-sess.verify=verify_flag
-sess.keep_alive = True
-
-# authentication
-req = Request('GET', 
-            url,
-            auth=(conf['site_user'], conf['site_pword']),
-)
-req.params={'type': 'sale', 'mode': 'checkauth', 'version': conf['version']}
-prepped = sess.prepare_request(req)
-resp = None
-try:
-    resp = sess.send(prepped)
-    logging.debug("checkauth prepped sent")
-    logging.debug("checkauth resp.status_code=%s", resp.status_code)
-    logging.debug("checkauth resp.text=%s", resp.text)
-
-    if 200 != resp.status_code:
-        logging.debug("checkauth code NEQ 200, sess.headers=%s, sess.params=%s", str(sess.headers),  str(sess.params))
-        exit(resp.status_code)
-
-    saved_cookies = resp.cookies
-    logging.debug("resp.cookies=%s", str(resp.cookies))
-
-    (auth_result, cookie_file, cookie_value, sessid) = resp.text.split()
-    logging.debug("Parsed by =")
-    logging.debug("auth_result=%s", auth_result)
-    logging.debug("cookie_file=%s", cookie_file)
-    logging.debug("cookie_value=%s", cookie_value)
-    logging.debug("sessid=%s", sessid)
-
-    # if authorized
-    if 'success' == auth_result:
-        logging.info("Authentication succeed!")
-        sess_id = sessid.split('=')
-        # initializing
-        req.params={'type': 'sale', 'mode': 'init', 'version': conf['version'], 'sessid': sess_id[1]}
-        prepped = sess.prepare_request(req)
-        sess.cookies = saved_cookies
+    # authentication
+    req = Request('GET',
+                  url,
+                  auth=(site_user, site_pword),
+                  )
+    req.params = {'type': 'sale', 'mode': 'checkauth', 'version': version}
+    prepped = sess.prepare_request(req)
+    resp = None
+    try:
         resp = sess.send(prepped)
-        logging.debug("init resp.text=%s", resp.text)
-        (zip_enabled, file_limit, sessid, version) = resp.text.split()
+        logging.debug("checkauth prepped sent")
+        logging.debug("checkauth resp.status_code=%s", resp.status_code)
+        logging.debug("checkauth resp.text=%s", resp.text)
 
-        # query orders from site
-        sess_id = sessid.split('=')
-        req.params={'type': 'sale', 'mode': 'query', 'version': conf['version'], 'sessid': sess_id[1]}
-        req.method='GET'
-        prepped = sess.prepare_request(req)
-        sess.cookies = saved_cookies
-        resp = sess.send(prepped)
-        xml_from_site = resp.text
+        resp.raise_for_status()
+        """
+        if 200 != resp.status_code:
+            logging.debug("checkauth code NEQ 200, sess.headers=%s, sess.params=%s", str(sess.headers), str(sess.params))
+            exit(resp.status_code)
+        """
 
+        saved_cookies = resp.cookies
+        logging.debug("resp.cookies=%s", str(resp.cookies))
 
-        if 200 == resp.status_code:
-            # send 'success'
-            req.params={'type': 'sale', 'mode': 'success', 'version': conf['version'], 'sessid': sess_id[1]}
+        (auth_result, cookie_file, cookie_value, sessid) = resp.text.split()
+        logging.debug("Parsed by =")
+        logging.debug("auth_result=%s", auth_result)
+        logging.debug("cookie_file=%s", cookie_file)
+        logging.debug("cookie_value=%s", cookie_value)
+        logging.debug("sessid=%s", sessid)
+
+        # if authorized
+        if 'success' == auth_result:
+            logging.info("Authentication succeed!")
+            sess_id = sessid.split('=')
+            # initializing
+            req.params = {'type': 'sale', 'mode': 'init', 'version': version, 'sessid': sess_id[1]}
             prepped = sess.prepare_request(req)
             sess.cookies = saved_cookies
             resp = sess.send(prepped)
-            logging.debug("success resp.text=%s", resp.text)
+            logging.debug("init resp.text=%s", resp.text)
+            (zip_enabled, file_limit, sessid, version) = resp.text.split()
 
-            xml_orders = get_xml_list_fixed(xml_from_site.splitlines())
-            logging.debug("len(xml_orders)=%s", len(xml_orders))
+            # query orders from site
+            sess_id = sessid.split('=')
+            req.params = {'type': 'sale', 'mode': 'query', 'version': version, 'sessid': sess_id[1]}
+            req.method = 'GET'
+            prepped = sess.prepare_request(req)
+            sess.cookies = saved_cookies
+            resp = sess.send(prepped)
+            xml_from_site = resp.text
 
-            if 4 == len(xml_orders):
-                logging.debug('empty xml, just header. Skip DB operation.')
-            else:
-                ver_num = '0'
-                for x in xml_orders:
-                    m_ver = re_ver_num.match(x.encode('utf-8'))
-                    if m_ver:
-                        ver_num = m_ver.group(1)
+            if 200 == resp.status_code:
+                # send 'success'
+                req.params = {'type': 'sale', 'mode': 'success', 'version': version, 'sessid': sess_id[1]}
+                prepped = sess.prepare_request(req)
+                sess.cookies = saved_cookies
+                resp = sess.send(prepped)
+                logging.debug("success resp.text=%s", resp.text)
 
-                xml_lines = u"\n".join(xml_orders)
-                fname_templ = conf['site'] + "-%Y-%m-%d_%H-%M-%S"
-                sql_outfile_name = time.strftime("02-sql/orders-" + fname_templ + ".sql")
-                xmlf_name = time.strftime("01-xml/orders-" + fname_templ + ".xml")
-                # xmlf_name = time.strftime("01-xml/orders-" +conf['site'] + "-%Y-%m-%d_%H-%M-%S.xml")
-                xmlf=codecs.open(xmlf_name, 'w', 'utf-8')
-                xmlf.write(xml_lines)
-                xmlf.close()
-                logging.info("wrote xml file: %s", xmlf_name)
+                xml_orders = get_xml_list_fixed(xml_from_site.splitlines())
+                logging.debug("len(xml_orders)=%s", len(xml_orders))
 
-except Exception as e:
-    logging.critical("exception=%s", str(e))
+                if 4 == len(xml_orders):
+                    logging.debug('empty xml, just header. Skip.')
+                else:
+                    xml_lines = u"\n".join(xml_orders)
+                    fname_templ = site + "-%Y-%m-%d_%H-%M-%S"
+                    xml_fname = time.strftime("01-xml/orders-" + fname_templ + ".xml")
+                    xmlf = open(xml_fname, 'w', 'utf-8')
+                    xmlf.write(xml_lines)
+                    xmlf.close()
+                    logging.info("wrote xml file: %s", xml_fname)
+
+    except Exception:
+        logging.critical("exception=%s", exc_info=True)
+        xml_lines = None
+    return xml_lines
+
+# ------------------- Main -------------------
+
+if __name__ == '__main__':
+    # TODO command line options: conf file, logging_level, order's status file
+    log_dir = ''
+    log_format = '[%(filename)-20s:%(lineno)4s - %(funcName)20s()] %(levelname)-7s | %(asctime)-15s | %(message)s'
+
+    (prg_name, prg_ext) = os.path.splitext(os.path.basename(__file__))
+    logging.basicConfig(filename=prg_name + '.log', filemode='a', format=log_format, level=logging.DEBUG)
+
+    conf = {}
+    execfile(prg_name + ".conf", conf)
+
+    site = conf['site']
+    site_user = conf['site_user']
+    site_pword = conf['site_pword']
+    version = conf['version']
+
+    if site.endswith('arc.world'):
+        verify_flag = False
+        proto = 'http://'
+    else:
+        verify_flag = True
+        proto = 'https://'
+
+    get_orders(site, site_user, site_pword, version)
 
 
-############## Bottom line #########################
+
+
+# ------------ Bottom line -------------------------
--- bin/bx_orders_parse.py
+++ bin/bx_orders_parse.py
@@ -5,25 +5,155 @@ import os
 import argparse
 import codecs
 import time
-import re
 
-import psycopg2
 import psycopg2.extensions
 import logging
-from xml.etree import ElementTree as ET
+from xml.etree import ElementTree
+
+from bx_orders_download import get_orders
+
+# ---------------- parse_xml -----------------------------------
+def parse_xml(xml_lines):
+    sql_orders = {}
+    pg_order_fields = (
+        u"Ид",
+        u"Номер",
+        u"Дата",
+        u"ХозОперация",
+        u"Роль",
+        u"Валюта",
+        u"Курс",
+        u"Сумма",
+        u"Время",
+        u"НомерВерсии"
+    )
+
+    pg_order_item_fields = (
+        u"Ид",
+        u"ИдКаталога",
+        u"Наименование",
+        u"Единица/Код",
+        u"Единица/НаименованиеПолное",
+        u"Коэффициент",
+        u"ЦенаЗаЕдиницу",
+        u"Количество",
+        u"Сумма",
+    )
+
+    el = ElementTree.fromstring(xml_lines.encode('utf-8'))
+    for bx_order in el.findall(u'Документ'):
+        sql_lines = []
+        logging.debug(bx_order.tag.encode('utf-8'))  # 'Документ'
+        bx_order_id = bx_order.find(u'Ид').text
+        cur.execute('SELECT 1 FROM bx_order WHERE "Номер" = {0};'.format(bx_order_id))
+        # if FOUND do not write sql into file, go to the next
+        if cur.fetchone() is not None:
+            logging.info("bx_order_id={0} is found in DB, skip".format(bx_order_id))
+            continue
+
+        bx_counterpart = bx_order.find(u'Контрагенты/Контрагент')
+        bx_buyer = bx_counterpart.find(u'Ид').text  # .encode('utf-8')
+        buyer = bx_buyer.split("#")  # 0-bx_buyer_id, 1-bx_buyer_login, 2-bx_buyer_name
+        bx_buyer_id = buyer[0]
+        bx_buyer_login = buyer[1].strip(' ')
+        bx_buyer_name = buyer[2].strip(' ')
+        flagFastOrder = 'OrderUser' in bx_buyer_login
+        bx_buyer_inn = bx_counterpart.find(u'ИНН')
+        bx_buyer_kpp = bx_counterpart.find(u'КПП')
+        loc_legal_entity = bx_buyer_inn is not None
+        logging.debug("bx_buyer_id={0}, юр.лицо={1}".format(bx_buyer_id, loc_legal_entity))
+        # TODO search buyer_id in DB
+        # insert OR update bx_buyer
+        buyer_sql = """INSERT INTO bx_buyer(bx_buyer_id, bx_logname, bx_name, "ИНН", "КПП") 
+                    VALUES ({0}, '{1}', '{2}', '{3}', '{4}')
+                    ON CONFLICT (bx_buyer_id)
+                    DO UPDATE SET 
+                    bx_name = EXCLUDED.bx_name
+                    , "ИНН" = EXCLUDED."ИНН"
+                    , "КПП" = EXCLUDED."КПП"
+                    ;\n""".format(bx_buyer_id, bx_buyer_login, bx_buyer_name, bx_buyer_inn, bx_buyer_kpp)
+        cur.execute(buyer_sql)
+
+        pg_order_vals = []
+
+        for f in pg_order_fields:
+            val = bx_order.find(f).text.encode('utf-8')
+            pg_order_vals.append(val)
+
+        flds = ', '.join(map(u'"{0}"'.format, pg_order_fields))
+        flds = u'bx_buyer_id, {0}'.format(flds)
+        pg_order_vals.insert(0, bx_buyer_id)
+        vals = ', '.join(map("'{0}'".format, pg_order_vals))
+        # logging.debug(flds)
+        # logging.debug(vals)
+        sql_bx_order = 'INSERT INTO bx_order({0}) VALUES({1});\n'.format(flds.encode('utf-8'), vals)
+        logging.debug(sql_bx_order)
+        # sqlf.write(sql_bx_order)
+        sql_lines.append(sql_bx_order)
+
+        for bx_order_feature in bx_order.findall(u'ЗначенияРеквизитов/ЗначениеРеквизита'):
+            bof_name = bx_order_feature.find(u'Наименование').text.encode('utf-8')
+            bof_value = bx_order_feature.find(u'Значение').text.encode('utf-8').replace('\r\n', '/').replace('\n',
+                                                                                                             '/').replace(
+                "'", "''")
+            insert_bx_order_feature = 'INSERT INTO bx_order_feature("bx_order_Номер", fname, fvalue)'
+            values_bx_order_feature = "VALUES({0}, '{1}', '{2}');\n".format(bx_order_id, bof_name, bof_value)
+            sql_bx_order_feature = "{0} {1}".format(insert_bx_order_feature, values_bx_order_feature)
+            logging.debug(sql_bx_order_feature)
+            # sqlf.write(sql_bx_order_feature)
+            sql_lines.append(sql_bx_order_feature)
+
+        for bx_item in bx_order.findall(u'Товары/Товар'):
+            pg_order_item_vals = []
+            bx_item_id = bx_item.find(u'Ид').text
+            logging.debug('bx_item_id={0}'.format(bx_item_id))
+            for f in pg_order_item_fields:
+                val = bx_item.find(f).text.encode('utf-8')
+                pg_order_item_vals.append(val)
+
+            flds = ', '.join(map(u'"{0}"'.format, pg_order_item_fields))
+            flds = u'bx_order_Номер, {0}'.format(flds)
+            pg_order_item_vals.insert(0, bx_order_id)
+            vals = ', '.join(map("'{0}'".format, pg_order_item_vals))
+            flds = flds.replace(u'Единица/', '')
+            # logging.debug(flds)
+            # logging.debug(vals)
+            sql_bx_order_item = 'INSERT INTO bx_order_item({0}) VALUES({1});\n'.format(flds.encode('utf-8'), vals)
+            logging.debug(sql_bx_order_item)
+            # sqlf.write(sql_bx_order_item)
+            sql_lines.append(sql_bx_order_item)
+
+            for bx_item_feature in bx_item.findall(u'ЗначенияРеквизитов/ЗначениеРеквизита'):
+                bif_name = bx_item_feature.find(u'Наименование').text.encode('utf-8')
+                bif_value = bx_item_feature.find(u'Значение').text.encode('utf-8').replace('\r\n', '/').replace('\n',
+                                                                                                                '/').replace(
+                    "'", "''")
+                insert_bx_order_item_feature = 'INSERT INTO bx_order_item_feature(bx_order_item_id, "bx_order_Номер", fname, fvalue)'
+                values_bx_order_item_feature = "VALUES({0}, {1}, '{2}', '{3}');\n".format(bx_item_id, bx_order_id,
+                                                                                          bif_name, bif_value)
+                sql_bx_order_item_feature = "{0} {1}".format(insert_bx_order_item_feature, values_bx_order_item_feature)
+                logging.debug(sql_bx_order_item_feature)
+                # sqlf.write(sql_bx_order_item_feature)
+                sql_lines.append(sql_bx_order_item_feature)
+
+        sql_orders[bx_order_id] = sql_lines
+
+    return sql_orders
 
 
-################## Main ###################################
 
-# TODO command line options: conf file, logging_level, order's status file
+# ---------------- Main -----------------------------------
+
 (prg_name, prg_ext) = os.path.splitext(os.path.basename(__file__))
-conf_file = prg_name +".conf"
-log_file = prg_name +".log"
+conf_file = prg_name + ".conf"
+log_file = prg_name + ".log"
 
 parser = argparse.ArgumentParser(description='Pg listener for .')
 parser.add_argument('--conf', type=str, default=conf_file, help='conf file')
-parser.add_argument('--xml_file', type=str, required=True, help='conf file')
+parser.add_argument('--xml_file', type=str, help='conf file')
 parser.add_argument('--log_level', type=str, default="DEBUG", help='log level')
+parser.add_argument('--run_sql', type=bool, default=False, help='run prepared sql')
+parser.add_argument('--create_bill', type=bool, default=False, help='run create_inet_bill')
 log_dir = ''
 args = parser.parse_args()
 
@@ -33,143 +163,58 @@ if not isinstance(numeric_level, int):
 
 log_format = '[%(filename)-20s:%(lineno)4s - %(funcName)20s()] %(levelname)-7s | %(asctime)-15s | %(message)s'
 
-logging.basicConfig(filename=log_file, filemode = 'a', format=log_format, level=numeric_level)
+logging.basicConfig(filename=log_file, filemode='a', format=log_format, level=numeric_level)
 
 
 conf = {}
 # execfile(prg_name+".conf", conf)
 execfile(args.conf, conf)
+site = conf['site']
 
-re_ver_num = re.compile(r'<НомерВерсии>(\d+)</НомерВерсии>')
+# password='XXXX' in .pgpass
+pg_con = psycopg2.connect("host='" + conf["pg_srv"] + "' dbname='arc_energo' user='arc_energo'")
+# TODO check return code
+cur = pg_con.cursor()
 
-"""
-con = psycopg2.connect("host='" + conf["pg_srv"] + "' dbname='arc_energo' user='arc_energo'") # password='XXXX' - .pgpass
-#TODO check return code
-"""
+if args.xml_file is None:
+    site_user = conf['site_user']
+    site_pword = conf['site_pword']
+    version = conf['version']
 
-xmlf=codecs.open(args.xml_file, 'r', 'utf-8')
-xml_lines = xmlf.read()
-xmlf.close()
-logging.info("read xml file: %s", args.xml_file)
+    if site.endswith('arc.world'):
+        verify_flag = False
+        proto = 'http://'
+    else:
+        verify_flag = True
+        proto = 'https://'
 
-bx_orders = []
-pg_order_fields = (
-    u"Ид",
-    u"Номер",
-    u"Дата",
-    u"ХозОперация",
-    u"Роль",
-    u"Валюта",
-    u"Курс",
-    u"Сумма",
-    u"Время",
-    u"НомерВерсии"
-)
+    xml_lines = get_orders(site, site_user, site_pword, version)
+else:
+    xmlf = codecs.open(args.xml_file, 'r', 'utf-8')
+    xml_lines = xmlf.read()
+    xmlf.close()
+    logging.info("read xml file: %s", args.xml_file)
 
-pg_order_item_fields = (
-    u"Ид",
-    u"ИдКаталога",
-    u"Наименование",
-    u"Единица/Код",
-    u"Единица/НаименованиеПолное",
-    u"Коэффициент",
-    u"ЦенаЗаЕдиницу",
-    u"Количество",
-    u"Сумма",
-)
-
-el = ET.fromstring(xml_lines.encode('utf-8'))
-# logging.info("xml_lines were parsed")
-for bx_order in el.findall(u'Документ'):
-    logging.debug(bx_order.tag.encode('utf-8')) # 'Документ'
-    bx_counterpart = bx_order.find(u'Контрагенты/Контрагент')
-    bx_buyer = bx_counterpart.find(u'Ид').text #.encode('utf-8')
-    buyer = bx_buyer.split("#") # 0-bx_buyer_id, 1-bx_buyer_login, 2-bx_buyer_name
-    bx_buyer_id = buyer[0]
-    bx_buyer_login = buyer[1].strip(' ')
-    bx_buyer_name = buyer[2].strip(' ')
-    flagFastOrder = 'OrderUser' in bx_buyer_login
-    bx_buyer_inn = bx_counterpart.find(u'ИНН')
-    bx_buyer_kpp = bx_counterpart.find(u'КПП')
-    loc_legal_entity = bx_buyer_inn is not None
-    logging.debug("bx_buyer_id={0}, юр.лицо={1}".format(bx_buyer_id, loc_legal_entity))
-    # TODO search buyer_id in DB
-    # insert OT update bx_buyer
-
-    pg_order_vals = []
-    doc_id = bx_order.find(u'Ид').text
-    # TODO search order_id in DB
-    # bx_orders.append(doc_id)
-    # if FOUND do not write sql into file, just comment
-
-    sql_outfile_name = "02-sql/order-{0}-{1}-{2}.sql".format(conf['site'], doc_id, time.strftime("%Y-%m-%d_%H-%M-%S"))
-    sqlf=open(sql_outfile_name, 'w')  # , 'utf-8')
-
-    for f in pg_order_fields:
-        val = bx_order.find(f).text.encode('utf-8')
-        pg_order_vals.append(val)
-
-    flds = ', '.join(map(u'"{0}"'.format, pg_order_fields))
-    flds = u'bx_buyer_id, {0}'.format(flds)
-    pg_order_vals.insert(0, bx_buyer_id)
-    vals = ', '.join(map("'{0}'".format, pg_order_vals))
-    # logging.debug(flds)
-    # logging.debug(vals)
-    sql_bx_order = 'INSERT INTO bx_order({0}) VALUES({1});\n'.format(flds.encode('utf-8'), vals)
-    logging.debug(sql_bx_order)
-    sqlf.write(sql_bx_order)
-
-    for bx_order_feature in bx_order.findall(u'ЗначенияРеквизитов/ЗначениеРеквизита'):
-        bof_name = bx_order_feature.find(u'Наименование').text.encode('utf-8')
-        bof_value = bx_order_feature.find(u'Значение').text.encode('utf-8').replace('\r\n','/').replace('\n','/').replace("'", "''")
-        insert_bx_order_feature = 'INSERT INTO bx_order_feature("bx_order_Номер", fname, fvalue)'
-        values_bx_order_feature = "VALUES({0}, '{1}', '{2}');\n".format(doc_id, bof_name, bof_value)
-        sql_bx_order_feature = "{0} {1}".format(insert_bx_order_feature, values_bx_order_feature)
-        logging.debug(sql_bx_order_feature)
-        sqlf.write(sql_bx_order_feature)
-    
-    for bx_item in bx_order.findall(u'Товары/Товар'):
-        pg_order_item_vals = []
-        bx_item_id = bx_item.find(u'Ид').text
-        logging.debug('bx_item_id={0}'.format(bx_item_id))
-        for f in pg_order_item_fields:
-            val = bx_item.find(f).text.encode('utf-8')
-            pg_order_item_vals.append(val)
-
-        flds = ', '.join(map(u'"{0}"'.format, pg_order_item_fields))
-        flds = u'bx_order_Номер, {0}'.format(flds)
-        pg_order_item_vals.insert(0, doc_id)
-        vals = ', '.join(map("'{0}'".format, pg_order_item_vals))
-        flds = flds.replace(u'Единица/', '')
-        # logging.debug(flds)
-        # logging.debug(vals)
-        sql_bx_order_item = 'INSERT INTO bx_order_item({0}) VALUES({1});\n'.format(flds.encode('utf-8'), vals)
-        logging.debug(sql_bx_order_item)
-        sqlf.write(sql_bx_order_item)
-
-        for bx_item_feature in bx_item.findall(u'ЗначенияРеквизитов/ЗначениеРеквизита'):
-            bif_name = bx_item_feature.find(u'Наименование').text.encode('utf-8')
-            bif_value = bx_item_feature.find(u'Значение').text.encode('utf-8').replace('\r\n','/').replace('\n','/').replace("'", "''")
-            insert_bx_order_item_feature = 'INSERT INTO bx_order_item_feature(bx_order_item_id, "bx_order_Номер", fname, fvalue)'
-            values_bx_order_item_feature = "VALUES({0}, {1}, '{2}', '{3}');\n".format(bx_item_id, doc_id, bif_name, bif_value)
-            sql_bx_order_item_feature = "{0} {1}".format(insert_bx_order_item_feature, values_bx_order_item_feature)
-            logging.debug(sql_bx_order_item_feature)
-            sqlf.write(sql_bx_order_item_feature)
-
+for bx_order_id, sql_order in parse_xml(xml_lines).iteritems():
+    logging.info('write to sql-file bx_order={0}'.format(bx_order_id))
+    sql_outfile_name = "02-sql/order-{0}-{1}-{2}.sql".format(site, bx_order_id,
+                                                             time.strftime("%Y-%m-%d_%H-%M-%S"))
+    sqlf = open(sql_outfile_name, 'w')
+    for sql_line in sql_order:
+        sqlf.write("%s\n" % sql_line)
     sqlf.close()
 
+    if args.run_sql:
+        sql_lines = "".join(sql_order)
+        cur.execute(sql_lines)
+        pg_con.commit()
+        if args.create_bill:
+            run_bxorder2bill = u'SELECT bxorder2bill('+ bx_order_id +u');'
+            cur.execute(run_bxorder2bill)
+            pg_con.commit()
 
-# logging.debug('bx_orders={0}'.format(bx_orders))
-
-
-# parse_xml_insert_into_db(conf['site'], el, con, sql_outfile_name)
-#logging.info("sql-file created: %s", sql_outfile_name)
-
-"""
-cur = con.cursor()
-con.commit()
+pg_con.commit()
 cur.close()
-"""
 
 
-############## Bottom line #########################
+# ------------ Bottom line -------------------------
