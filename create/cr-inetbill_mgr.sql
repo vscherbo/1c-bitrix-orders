8ae0c8e195ee2ace33307e15cc474b3272db68da
Vladimir Scherbo <vscherbo@gmail.com>
2018-05-18 12:50:08 +0300

    выбор с учётом флага Активность

--- create/cr-inetbill_mgr.sql
+++ create/cr-inetbill_mgr.sql
@@ -3,7 +3,16 @@ returns integer
 language sql
 as
 $body$
-    -- 
-    select 38 as result;
+    -- select 38 as result;
     -- select 89 as result;
+select "Менеджер" as result from (
+SELECT 10 as priority, "Менеджер" FROM "Сотрудники" where "Активность" = true and "Менеджер" = 38
+union 
+SELECT 20 as priority, "Менеджер" FROM "Сотрудники" where "Активность" = true and "Менеджер" = 89
+union 
+SELECT 30 as priority, "Менеджер" FROM "Сотрудники" where "Активность" = true and "Менеджер" = 12
+union 
+SELECT 40 as priority, "Менеджер" FROM "Сотрудники" where "Активность" = true and "Менеджер" = 41
+ORDER BY priority LIMIT 1) im
+
 $body$
