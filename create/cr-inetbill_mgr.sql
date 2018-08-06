44a4bd2e765d2ce6a93fde08c29cfd490e5c6e13
Vladimir Scherbo <vscherbo@gmail.com>
2018-08-06 14:54:36 +0300

    комментарии

--- create/cr-inetbill_mgr.sql
+++ create/cr-inetbill_mgr.sql
@@ -6,13 +6,13 @@ $body$
     -- select 38 as result;
     -- select 89 as result;
 select "Менеджер" as result from (
-SELECT 10 as priority, "Менеджер" FROM "Сотрудники" where "Активность" = true and "Менеджер" = 38
+SELECT 10 as priority, "Менеджер" FROM "Сотрудники" where "Активность" = true and "Менеджер" = 38 -- Гараханян
 union 
-SELECT 20 as priority, "Менеджер" FROM "Сотрудники" where "Активность" = true and "Менеджер" = 89
+SELECT 20 as priority, "Менеджер" FROM "Сотрудники" where "Активность" = true and "Менеджер" = 89 -- Хапёрский
 union 
-SELECT 30 as priority, "Менеджер" FROM "Сотрудники" where "Активность" = true and "Менеджер" = 12
+SELECT 30 as priority, "Менеджер" FROM "Сотрудники" where "Активность" = true and "Менеджер" = 12 -- Шалаевский
 union 
-SELECT 40 as priority, "Менеджер" FROM "Сотрудники" where "Активность" = true and "Менеджер" = 41
+SELECT 40 as priority, "Менеджер" FROM "Сотрудники" where "Активность" = true and "Менеджер" = 41 -- Робот ???
 ORDER BY priority LIMIT 1) im
 
 $body$
