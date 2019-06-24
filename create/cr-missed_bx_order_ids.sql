241f1c85f5144dd75ad0afc9e9261ef99855efb5
Vladimir Scherbo <vscherbo@gmail.com>
2019-06-24 12:09:19 +0300

    прекратить принудительно обновлять заказ, если были ошибки обновления

--- create/cr-missed_bx_order_ids.sql
+++ create/cr-missed_bx_order_ids.sql
@@ -10,8 +10,10 @@ WHERE   NOT EXISTS
         FROM    bx_order bi
         WHERE   bi."Номер" = bo."Номер" + 1
         )
-and "Номер" + 1 not in (select bx_order_id from bx_order_missed  where status = 1)        
+and "Номер" + 1 not in (SELECT bx_order_id FROM bx_order_missed WHERE status = 1)
+-- например, заказ отсутствует на сайте. Требуется ручное разбирательство
+and "Номер" + 1 not in (SELECT DISTINCT io_id FROM inet_orders_status_queue WHERE io_update_result = 1)
 and bo."Дата" = current_date
-ORDER by "Номер" desc 
+ORDER by "Номер" desc
 offset 1;
 $function$
