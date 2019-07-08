1c65ce80a42a5eecdd9d4ee087403b32f7e6b4bd
Vladimir Scherbo <vscherbo@gmail.com>
2019-07-08 12:37:18 +0300

    если был день без заказов

--- create/cr-missed_bx_order_ids.sql
+++ create/cr-missed_bx_order_ids.sql
@@ -13,7 +13,9 @@ WHERE   NOT EXISTS
 and "Номер" + 1 not in (SELECT bx_order_id FROM bx_order_missed WHERE status = 1)
 -- например, заказ отсутствует на сайте. Требуется ручное разбирательство
 and "Номер" + 1 not in (SELECT DISTINCT io_id FROM inet_orders_status_queue WHERE io_update_result = 1)
-and bo."Дата" = current_date
+-- and bo."Дата" = current_date
+-- с учётом выходных без заказов
+and bo."Дата" >= current_date - '2 days'::interval
 ORDER by "Номер" desc
 offset 1;
 $function$
