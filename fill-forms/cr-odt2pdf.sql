ecebb28f350a688c0454e98c297623c6d1fd1525
Vladimir Scherbo <vscherbo@gmail.com>
2020-02-12 15:00:15 +0300

    pdf_server; new args: indir, outdir with DEFAULT

--- fill-forms/cr-odt2pdf.sql
+++ fill-forms/cr-odt2pdf.sql
@@ -1,22 +1,30 @@
 -- Function: odt2pdf(character varying)
+drop Function odt2pdf(character varying);
+-- drop Function odt2pdf(character varying, varchar, varchar);
 
 
-CREATE OR REPLACE FUNCTION arc_energo.odt2pdf(input_file character varying)
+CREATE OR REPLACE FUNCTION arc_energo.odt2pdf(input_file character varying,
+  indir VARCHAR DEFAULT '/opt/autobill/db',
+  outdir VARCHAR DEFAULT '/opt/autobill/output'
+)
   RETURNS character varying AS
 $BODY$DECLARE
   cmd VARCHAR;
   res_exec RECORD;
   str_res VARCHAR;
   out_fname VARCHAR;
-  indir VARCHAR = '/opt/autobill/db';
-  outdir VARCHAR = '/opt/autobill/output';
+  -- indir VARCHAR = '/opt/autobill/db';
+  -- outdir VARCHAR = '/opt/autobill/output';
   basename VARCHAR;
+  loc_pdf_server varchar;
 BEGIN
 -- libreoffice --headless --convert-to pdf /opt/autobill/data/39200389-Бланк-заказа.odt
 basename = regexp_replace(input_file, '^.+[/\\]', '');
 cmd := E'libreoffice --headless --convert-to pdf --outdir ' || outdir || ' ' || indir || '/' || basename;
-RAISE NOTICE 'odt2pdf cmd=%', cmd;
-res_exec := public.exec_paramiko('ct-apps01.arc.world', 22, 'autobill'::VARCHAR, cmd);
+loc_pdf_server := arc_energo.arc_const('pdf_server');
+RAISE NOTICE 'odt2pdf server=%, cmd=%', loc_pdf_server, cmd;
+res_exec := public.exec_paramiko(loc_pdf_server, 22, 'autobill'::VARCHAR, cmd);
+-- res_exec := public.exec_paramiko('ct-apps01.arc.world', 22, 'autobill'::VARCHAR, cmd);
 
 RAISE NOTICE 'odt2pdf res_exec=%', res_exec;
 
